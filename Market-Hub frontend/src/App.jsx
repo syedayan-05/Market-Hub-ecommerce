3c6feedfdea993af805a0d49a2819080113c3934
Syed Ayan <syedayan4128@gmail.com>
2025-11-01 03:09:56 +0530

    Remove Router wrapper and update route elements in App component

--- Market-Hub frontend/src/App.jsx
+++ Market-Hub frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/shop" element={<h1>Shop Page</h1>} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
