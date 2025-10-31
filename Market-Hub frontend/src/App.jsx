4ec55f462736f1707d3cddf35372836a04b19eac
Syed Ayan <syedayan4128@gmail.com>
2025-11-01 03:12:37 +0530

    Add tailwindcss-animate dependency and update App component with Tailwind styling

--- Market-Hub frontend/src/App.jsx
+++ Market-Hub frontend/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <Navbar />
+      <h1 className="text-4xl font-bold text-blue-600 text-center mt-10">
+  Tailwind is Working 🚀
+</h1>
+
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/shop" element={<h1>Shop Page</h1>} />
--- Market-Hub frontend/tailwind.config.js
+++ Market-Hub frontend/tailwind.config.js
@@ -5,17 +5,8 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  extend: {
-    colors: {
-      primary: '#FF9900', // Amazon orange
-      dark: '#131921', // header dark
-      light: '#232f3e',
-      accent: '#febd69',
-    },
-    boxShadow: {
-      card: '0 4px 12px rgba(0,0,0,0.1)',
-    },
+    extend: {},
   },
-},
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
+
 }
